d7c78cd26bb17605b286910ebac83013a0c8b7b0
DESKTOP-7E3R1CO\David <davidricardolopezbarrios@gmail.com>
2021-01-23 19:04:43 -0500

    Modifica main.c para crear ejemplo de utilizacion de nueva libreria UART0

--- MKL02Z32xxx4_Project_mma8451/source/MKL02Z32xxx4_Project_mma8451.c
+++ MKL02Z32xxx4_Project_mma8451/source/MKL02Z32xxx4_Project_mma8451.c
@@ -39,16 +39,14 @@
 #include "clock_config.h"
 #include "MKL02Z4.h"
 #include "fsl_debug_console.h"
-/* TODO: insert other include files here. */
 
-/* TODO: insert other definitions and declarations here. */
+#include "sdk_hal_gpio.h"
+#include "sdk_hal_uart0.h"
 
-/*
- * @brief   Application entry point.
- */
 int main(void) {
 
-	status_t resultado;
+	status_t status;
+	uint8_t nuevo_byte_uart;
 
   	/* Init board hardware. */
     BOARD_InitBootPins();
@@ -59,22 +57,42 @@ int main(void) {
     BOARD_InitDebugConsole();
 #endif
 
-    PRINTF("Hello World\n");
+    (void)uart0Inicializar(115200);
 
-    //Coloca el pin PTB7 en alto
-    resultado=gpioPutLow(KPTB7);
+    PRINTF("Usar teclado para controlar LEDS\r\n");
+    PRINTF("r-R led ROJO\r\n");
+    PRINTF("v-V led VERDE\r\n");
+    PRINTF("a-A led AZUL\r\n");
 
-    if(resultado!=kStatus_Success)
-    	printf("error de operacion");
+	while (1) {
+		if (uart0CuantosDatosHayEnBuffer() > 0) {
+			status = uart0LeerByteDesdeBuffer(&nuevo_byte_uart);
+			if (status == kStatus_Success) {
+				printf("dato:%c\r\n", nuevo_byte_uart);
+				switch (nuevo_byte_uart) {
+				case 'a':
+				case 'A':
+					gpioPutToggle(KPTB10);
+					break;
 
-    /* Force the counter to be placed into memory. */
-    volatile static int i = 0 ;
-    /* Enter an infinite loop, just incrementing a counter. */
-    while(1) {
-        i++ ;
-        /* 'Dummy' NOP to allow source level single stepping of
-            tight while() loop */
-        __asm volatile ("nop");
-    }
+				case 'v':
+					gpioPutHigh(KPTB7);
+					break;
+				case 'V':
+					gpioPutLow(KPTB7);
+					break;
+
+				case 'r':
+					gpioPutValue(KPTB6, 1);
+					break;
+				case 'R':
+					gpioPutValue(KPTB6, 0);
+					break;
+				}
+			} else {
+				printf("error\r\n");
+			}
+		}
+	}
     return 0 ;
 }
